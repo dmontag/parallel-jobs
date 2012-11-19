0803cd84977de54255938f965bdc360a29dc8a6b
David Montag <david.montag@neotechnology.com>
2012-11-18 22:42:42 -0800

    Count rels via nodes job

---
+++ src/main/java/org/neo4j/parallel/jobs/CountRelsViaNodes.java
@@ -0,0 +1,103 @@
+package org.neo4j.parallel.jobs;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.kernel.InternalAbstractGraphDatabase;
+import org.neo4j.kernel.impl.nioneo.store.NodeRecord;
+import org.neo4j.kernel.impl.nioneo.store.RecordStore;
+import org.neo4j.kernel.impl.nioneo.store.RelationshipRecord;
+import org.neo4j.kernel.impl.nioneo.store.StoreAccess;
+import org.neo4j.parallel.Job;
+import org.neo4j.parallel.JobFactory;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+public class CountRelsViaNodes implements Job {
+
+    private GraphDatabaseService graphDb;
+    private PrintStream out;
+    private ExecutorService executorService;
+    private final int threads;
+    private volatile long actualTotal;
+    private final List<Future<Long>> futures = new ArrayList<Future<Long>>();
+
+    public CountRelsViaNodes(GraphDatabaseService graphDb, PrintStream out, int threads) {
+        this.graphDb = graphDb;
+        this.out = out;
+        this.threads = threads;
+        executorService = new ScheduledThreadPoolExecutor(this.threads);
+        actualTotal = 0;
+    }
+
+    public void process() throws Exception {
+        StoreAccess storeAccess = new StoreAccess((InternalAbstractGraphDatabase) graphDb);
+        final RecordStore<NodeRecord> nodeStore = storeAccess.getNodeStore();
+        final RecordStore<RelationshipRecord> relStore = storeAccess.getRelationshipStore();
+        long totalNumberOfNodes = nodeStore.getHighId();
+        final long nodesPerThread = totalNumberOfNodes / threads;
+        for (int i = 0; i < threads; i++) {
+            final int ii = i;
+            futures.add(executorService.submit(new Callable<Long>() {
+                public Long call() throws Exception {
+                    long localCount = 0;
+                    final long limit = (ii + 1) * nodesPerThread;
+                    for (long id = ii*nodesPerThread; id < limit; id++) {
+                        NodeRecord record = nodeStore.forceGetRaw(id);
+                        if (record.inUse()) {
+                            long nextRel = record.getNextRel();
+                            while (nextRel != -1) {
+                                RelationshipRecord relRecord = relStore.forceGetRaw(nextRel);
+                                if (relRecord.getFirstNode() == id) {
+                                    nextRel = relRecord.getFirstNextRel();
+                                } else {
+                                    nextRel = relRecord.getSecondNextRel();
+                                }
+                                localCount++;
+                            }
+                        }
+                    }
+                    return localCount;
+                }
+            }));
+        }
+        for (Future<Long> future : futures) {
+            actualTotal += future.get();
+        }
+        executorService.shutdown();
+    }
+
+    public void reportProgress() {
+        int numberOfRunningThreads = 0;
+        for (Future<Long> future : futures) {
+            if (!future.isDone()) numberOfRunningThreads++;
+        }
+        out.println(String.format(
+                "Total threads: %d\n" +
+                "Running threads: %d\n" +
+                "Tally so far: %d", threads, numberOfRunningThreads, actualTotal));
+    }
+
+    public void abort() {
+        executorService.shutdownNow();
+    }
+
+    public static class CountRelsViaNodesFactory implements JobFactory {
+
+        public Job getProcessor(GraphDatabaseService graphDb, List<String> args, PrintStream out) {
+            return new CountRelsViaNodes(graphDb, out, Integer.parseInt(args.get(0)));
+        }
+
+        public String name() {
+            return "countrelsvianodes";
+        }
+
+        public String argsHelp() {
+            return "<threads>";
+        }
+    }
+}
